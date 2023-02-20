aee44792e1c5ed9fa59b111c094faa32a41b5c4e
˜Edlane <edlane.santana@mercadolivre.com>
2023-02-20 15:49:23 -0300

    Inicializando o projeto

---
+++ src/main/java/com/org/queenstore/QueenstoreApplication.java
@@ -0,0 +1,13 @@
+package com.org.queenstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueenstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueenstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/queenstore/QueenstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.queenstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueenstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
